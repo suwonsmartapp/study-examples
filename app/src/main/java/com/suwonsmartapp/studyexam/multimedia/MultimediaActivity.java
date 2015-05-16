e5f28b535092606ff04a5f429a1193362b22ed29
Junsuk Oh <a811219@gmail.com>
2015-05-16 22:45:05 +0900

    메세지를 카톡 등에 보내는 방법

--- app/src/main/java/com/suwonsmartapp/studyexam/multimedia/MultimediaActivity.java
+++ app/src/main/java/com/suwonsmartapp/studyexam/multimedia/MultimediaActivity.java
@@ -4,12 +4,14 @@ import com.suwonsmartapp.studyexam.R;
 
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import java.io.IOException;
@@ -22,11 +24,30 @@ public class MultimediaActivity extends ActionBarActivity implements View.OnClic
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_multimedia);
 
+        // 다른 곳에서 호출 되었을 때 null 이 아님
+        Intent intent = getIntent();
+        if (intent != null) {
+            Uri uri = intent.getData();
+            try {
+                if (uri != null) {
+                    playMusic(uri);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
         // 삐비비빅 (local 파일 직접 재생)
         findViewById(R.id.btn_play).setOnClickListener(this);
 
         // 파일 선택
         findViewById(R.id.btn_pick).setOnClickListener(this);
+
+        // 메세지 받는 앱 호출하여 메세지 보내기
+        findViewById(R.id.btn_send_message).setOnClickListener(this);
+
+        // 카톡으로 메세지 보내기
+        findViewById(R.id.btn_send_message_kakao).setOnClickListener(this);
     }
 
     @Override
@@ -46,6 +67,26 @@ public class MultimediaActivity extends ActionBarActivity implements View.OnClic
                     Toast.makeText(getApplicationContext(), "실행 할 앱이 없습니다.", Toast.LENGTH_SHORT).show();
                 }
                 break;
+
+            case R.id.btn_send_message:
+                // Chooser를 통한 메세지 전송
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("text/plain");
+                String message = ((EditText)findViewById(R.id.et_message)).getText().toString();
+                intent.putExtra(Intent.EXTRA_TEXT, message);
+                startActivity(intent);
+                break;
+
+            case R.id.btn_send_message_kakao:
+                // 카톡으로 메세지 전송
+                PackageManager pm = getPackageManager();
+                Intent kakaoIntent = pm.getLaunchIntentForPackage("com.kakao.talk");
+                kakaoIntent.setAction(Intent.ACTION_SEND);
+                kakaoIntent.setType("text/plain");
+                String kakaoMessage = ((EditText)findViewById(R.id.et_message)).getText().toString();
+                kakaoIntent.putExtra(Intent.EXTRA_TEXT, kakaoMessage);
+                startActivity(kakaoIntent);
+                break;
         }
     }
     MediaPlayer mediaPlayer;
@@ -58,19 +99,22 @@ public class MultimediaActivity extends ActionBarActivity implements View.OnClic
             Uri dataUri = data.getData();
             Toast.makeText(getApplicationContext(), dataUri.toString(), Toast.LENGTH_SHORT).show();
 
-            if (mediaPlayer != null) {
-                mediaPlayer.release();
-                mediaPlayer = null;
-            }
             try {
-                mediaPlayer = new MediaPlayer();
-                mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
-                mediaPlayer.setDataSource(getApplicationContext(), dataUri);
-                mediaPlayer.prepare();
-                mediaPlayer.start();
+                playMusic(dataUri);
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
+    }
+
+    private void playMusic(Uri dataUri) throws IOException {
+        if (mediaPlayer != null) {
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+        mediaPlayer = new MediaPlayer();
+        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        mediaPlayer.setDataSource(getApplicationContext(), dataUri);
+        mediaPlayer.start();
     }
 }
